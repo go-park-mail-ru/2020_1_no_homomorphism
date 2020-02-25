487694c1e6cfce98784cc8e34213510d51e9be73
bulletmys <bulletmys@yandex.ru>
2020-02-25 21:47:32 +0300

    [ADD] basic POST image handler

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 
@@ -33,11 +35,11 @@ type MyHandler struct {
 
 func NewMyHandler() *MyHandler {
 	return &MyHandler{
-		Sessions: make(map[uuid.UUID]string, 10),
+		Sessions: make(map[string]uuid.UUID, 10),
 		Users: map[string]*models.User{
 			"test": {
-				Id:     uuid.FromStringOrNil("1"),
-				Nickname:   "test",
+				Id:       uuid.FromStringOrNil("1"),
+				Nickname: "test",
 				Password: "123",
 			},
 		},
@@ -94,7 +96,7 @@ func (api *MyHandler) loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	id := uuid.NewV4()
-	api.Sessions[userModel.Username] = id
+	api.Sessions[id.String()] = id
 	api.mu.Unlock()
 
 	cookie := &http.Cookie{
@@ -116,8 +118,6 @@ func (api *MyHandler) logoutHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	userToken, err := uuid.FromString(id.Value)
-
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusUnauthorized)
@@ -134,7 +134,6 @@ func (api *MyHandler) logoutHandler(w http.ResponseWriter, r *http.Request) {
 
 	id.Expires = time.Now().AddDate(0, 0, -1)
 	http.SetCookie(w, id)
-
 }
 
 func (api *MyHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
@@ -172,6 +171,47 @@ func (api *MyHandler) signUpHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 
+func (api *MyHandler) saveImageHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(5 * 1024 * 1025)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+
+	file, handler, err := r.FormFile("my_file")
+	if err != nil || handler.Size == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fileBody, err := ioutil.ReadAll(file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+
+	filePath := "./images/" + uuid.NewV4().String() + ".png" //todo подставлять формат файла
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+
+	_, err = newFile.Write(fileBody)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 	r := mux.NewRouter()
 	api := NewMyHandler()
@@ -181,6 +221,7 @@ func main() {
 	r.HandleFunc("/login", api.loginHandler).Methods("POST")
 	r.HandleFunc("/logout", api.loginHandler).Methods("DELETE")
 	r.HandleFunc("/signup", api.signUpHandler).Methods("POST")
+	r.HandleFunc("/image", api.saveImageHandler).Methods("POST")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		fmt.Println(err)
