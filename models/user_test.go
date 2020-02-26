a9be71f3c574c5bbfa8984587fc54572e7b7daf4
mortawe <mortawe@gmail.com>
2020-02-27 02:13:36 +0300

    фикс тестов

--- models/user_test.go
+++ models/user_test.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+func Test_NewUsersStorage(t *testing.T) {
+	mu := &sync.Mutex{}
+	_, err := NewUsersStorage(nil)
+	assert.NotNil(t, err)
+	_, err = NewUsersStorage(mu)
+	assert.Nil(t, err)
+}
