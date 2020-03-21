7fb2e3d5c3fd2c3f2108de8fa530560d51c76d2b
bulletmys <bulletmys@yandex.ru>
2020-03-21 22:35:05 +0300

    [ADD] playlists usecase

---
+++ internal/pkg/playlist/usecase.go
@@ -0,0 +1,8 @@
+package playlist
+
+import "no_homomorphism/internal/pkg/models"
+
+type UseCase interface {
+	GetUserPlaylists(id string) (*models.UserPlaylists, error)
+	GetPlaylistWithTracks(id string) (*models.PlaylistTracks, error)
+}
---
+++ internal/pkg/playlist/usecase/usecase.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"fmt"
+	"no_homomorphism/internal/pkg/models"
+	"no_homomorphism/internal/pkg/playlist"
+	"no_homomorphism/internal/pkg/track"
+	"strconv"
+)
+
+type PlaylistUseCase struct {
+	PlRepository    playlist.Repository
+	TrackRepository track.Repository
+}
+
+func (uc PlaylistUseCase) GetUserPlaylists(id string) (*models.UserPlaylists, error) {
+	plId, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert id: %e", err)
+	}
+	playlists, err := uc.PlRepository.GetUserPlaylists(plId)
+	return &models.UserPlaylists{
+		Count:     len(playlists),
+		Playlists: playlists,
+	}, err
+}
+
+func (uc PlaylistUseCase) GetPlaylistWithTracks(id string) (*models.PlaylistTracks, error) {
+	plId, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert id: %s", err)
+	}
+	dbPlaylist, err := uc.PlRepository.GetPlaylistById(plId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dbPlaylist: %s", err)
+	}
+
+	tracks, err := uc.TrackRepository.GetPlaylistTracks(plId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get dbPlaylist' tracks: %s", err)
+	}
+
+	return &models.PlaylistTracks{
+		Playlist: dbPlaylist,
+		Count:    len(tracks),
+		Tracks:   tracks,
+	}, nil
+}
