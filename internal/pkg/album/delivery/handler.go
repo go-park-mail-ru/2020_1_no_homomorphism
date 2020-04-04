629fda4911390031ce7840e475621fd366dacc69
bulletmys <bulletmys@yandex.ru>
2020-04-05 02:00:09 +0300

    [ADD] bounded func + replaced handler

--- internal/pkg/album/delivery/handler.go
+++ internal/pkg/album/delivery/handler.go
@@ -125,3 +125,63 @@ func (h *AlbumHandler) GetBoundedAlbumsByArtistId(w http.ResponseWriter, r *http
 
 	h.Log.HttpInfo(r.Context(), "OK", http.StatusOK)
 }
+
+func (h *AlbumHandler) GetAlbumTracks(w http.ResponseWriter, r *http.Request) {
+	varId, ok := mux.Vars(r)["id"]
+	if !ok {
+		h.Log.HttpInfo(r.Context(), "no id in mux vars", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	playlists, err := h.TrackUC.GetTracksByAlbumId(varId)
+	if err != nil {
+		h.Log.HttpInfo(r.Context(), "failed to get album' track"+err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	writer := json.NewEncoder(w)
+	err = writer.Encode(playlists)
+	if err != nil {
+		h.Log.HttpInfo(r.Context(), "can't write album' track into json:"+err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	h.Log.HttpInfo(r.Context(), "OK", http.StatusOK)
+}
+
+func (h *AlbumHandler) GetBoundedAlbumTracks(w http.ResponseWriter, r *http.Request) {
+	id, okId := r.Context().Value("id").(string)
+	start, okStart := r.Context().Value("start").(uint64)
+	end, okEnd := r.Context().Value("end").(uint64)
+
+	if !okId || !okStart || !okEnd {
+		h.Log.LogWarning(r.Context(), "playlist delivery", "GetBoundedPlaylistTracks", "failed to get vars")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	tracks, err := h.TrackUC.GetBoundedTracksByAlbumId(id, start, end)
+	if err != nil {
+		h.Log.HttpInfo(r.Context(), "failed to get tracks"+err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(struct {
+		Id     string         `json:"id"`
+		Tracks []models.Track `json:"tracks"`
+	}{id, tracks})
+
+	if err != nil {
+		h.Log.LogWarning(r.Context(), "tracks delivery", "GetBoundedAlbumTracks", "failed to encode json"+err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	h.Log.HttpInfo(r.Context(), "OK", http.StatusOK)
+}
